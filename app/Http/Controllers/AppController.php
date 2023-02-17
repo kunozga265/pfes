6da624c3cf4264557e1e2d968678f857c588b937
kunozga265 <kunozgamlowoka@gmail.com>
2023-02-17 14:52:54 +0200

    Checks if it is an API Call

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class AppController extends Controller
 {
+    public function isApi(Request $request)
+    {
+        //get cookie object
+        $CSRF_TOKEN=$request->cookie();
+        return !(count($CSRF_TOKEN) == 0);
+    }
 
 }
